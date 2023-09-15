c3ce0d0ab30e7742d07543dd017b973562fa81d4
trietng <93473943+trietng@users.noreply.github.com>
2023-09-15 16:47:14 +0700

    add back button to ReadArticleActivity

--- app/src/main/java/com/example/travelapp/ui/ReadArticleActivity.kt
+++ app/src/main/java/com/example/travelapp/ui/ReadArticleActivity.kt
@@ -2,9 +2,9 @@ package com.example.travelapp.ui
 
 import android.os.Bundle
 import android.webkit.WebView
-import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.example.travelapp.R
+import com.google.android.material.button.MaterialButton
 
 class ReadArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +13,17 @@ class ReadArticleActivity : AppCompatActivity() {
         val url = intent.getStringExtra("url")!!
         val webView = findViewById<WebView>(R.id.read_article_web_view)
         webView.loadUrl(url)
-        onBackPressedDispatcher.addCallback(this) {
+        val backButton = findViewById<MaterialButton>(R.id.read_article_back_button)
+        backButton.setOnClickListener {
             finish()
+        }
+        // If user scroll down gradually hide the back button and vice versa
+        webView.setOnScrollChangeListener { _, _, scrollY, _, _ ->
+            if (scrollY > 0) {
+                backButton.animate().alpha(0f).setDuration(200).start()
+            } else {
+                backButton.animate().alpha(1f).setDuration(200).start()
+            }
         }
     }
 }
